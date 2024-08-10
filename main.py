edac891b121353a9dd4a21df5b6f810921639087
BartoszP <bartosz12360@gmail.com>
2024-08-10 12:08:18 +0200

    Stworzono podstawowe menu

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# %% STRONA GLOWNA
 @app.route("/menu")
 def menu(): 
     return render_template("menu.html")
 
 
+# %% DODAWANIE ZGLOSZENIA 
+@app.route("/dodaj-zgloszenie", methods = ["POST"])
+def dodaj_zgloszenie():
+    pass
+
 
 if __name__ == "__main__":
     app.run()
+# %%
