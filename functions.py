7e1fd42b287e99e72cd8ec675d17bfd9f4e64c44
BartoszP <bartosz12360@gmail.com>
2024-08-19 22:28:50 +0200

    Dodawanie zgloszen i renderowanie danych, zapisywanie do pliku json

--- functions.py
+++ functions.py
@@ -1,8 +1,8 @@
 import json
 
 def save_data(data):
-    with open("zgloszenia.json", 'w') as file:
-        return json.dump(data, file, indent = 4)
+    with open("zgloszenia.json", 'w', encoding='utf-8') as file:
+        return json.dump(data, file, ensure_ascii=False,  indent = 4)
         
         
 def load_data():
--- main.py
+++ main.py
@@ -11,37 +11,41 @@ def menu():
 
 
 # %% DODAWANIE ZGLOSZENIA 
-@app.route('/dodaj-zgloszenie')
+@app.route('/dodaj-zgloszenie', methods=["GET", "POST"])
 def dodaj_zgloszenie():
+    if request.method == "POST":
+        user = request.form.get('user')
+        department = request.form.get('department')
+        machine = request.form.get('machine')
+        priority_level = request.form.get('priority_level')
+        reporting_date = request.form.get('date')
+        message = request.form.get('message')
+
+        data = load_data()
+        
+        if data:
+            max_task_id = max(int(task["task_id"]) for task in data)
+            new_task_id = max_task_id + 1
+        else:
+            new_task_id = 1
+        
+        new_task = {
+            'task_id': new_task_id,
+            'user': user,
+            'department': department,
+            'machine': machine,
+            'priority_level': priority_level,
+            'reporting_date': reporting_date,
+            'message': message
+        }
+        
+        data.append(new_task)
+        save_data(data)
+        
+        return render_template('success.html', new_task=new_task)
+
     return render_template('dodaj_zgloszenie.html')
 
-
-# %% New ID kod 
-@app.route("/dodaj-zgloszenie2", methods = ["POST"])
-def dodaj_zgloszenie2():
-    
-    if request.json:
-        new_task = request.json
-    else:
-        return jsonify({"message": "No input provided"}), 400
-    
-    data = load_data()
-    new_task_id = []
-    
-    if data:
-        max_task_id = max(int(task["task_id"]) for task in data)
-        new_task_id = max_task_id + 1
-    else:
-        new_task_id = 1
-    new_task['task_id'] = new_task_id
-    
-    
-    
-    data.append(new_task)
-    save_data(data)
-    # return jsonify({"message": "Task successfully added!", "task" : new_task}), 201
-    return render_template("dodaj_zgloszenie.html")
-    
 # %% ZGLOSZENIA --> LISTA ZGLOSZEN
 
 @app.route("/zgloszenia", methods = ["GET"])
@@ -57,23 +61,26 @@ def task_list():
 # %%  TEST 1 
 @app.route('/')
 def form():
-    return render_template('form.html')
+    return render_template('dodaj_zgloszenie.html')
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    name = request.form['name']
-    email = request.form['email']
-    age = request.form['age']
+    user = request.form['user']
+    department = request.form['department']
+    machine = request.form['machine']
+    priority_level = request.form['priority_level']
+    reporting_date = request.form['reporting_date']
     message = request.form['message']
     
     return f'''
         <h1>Dziękujemy za przesłanie danych!</h1>
-        <p><strong>Imię:</strong> {name}</p>
-        <p><strong>Email:</strong> {email}</p>
-        <p><strong>Wiek:</strong> {age}</p>
-        <p><strong>Wiadomość:</strong> {message}</p>
+        <p><strong> User:</strong> {user}</p>
+        <p><strong> Department:</strong> {department}</p>
+        <p><strong> Machine:</strong> {machine}</p>
+        <p><strong> Wiadomość:</strong> {priority_level}</p>
+        <p><strong> reporting_date: </strong> {reporting_date}</p>
+        <p><strong> Message: </strong> {message}</p>
     '''
-    
     
 if __name__ == "__main__":
     app.run()
