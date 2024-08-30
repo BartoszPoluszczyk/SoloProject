cc8836553f8351de2abb5e8ac2c368a70c5391dd
BartoszP <bartosz12360@gmail.com>
2024-08-30 14:20:01 +0200

    Poprawki w panelu rejestrowania- Python. Rozpoczecie panelu logowania

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import json
 from functions import save_data, load_data, load_users_data, save_users_data
 from collections import OrderedDict
+import werkzeug.security
 
 app = Flask(__name__)
 
@@ -30,30 +31,59 @@ def rejestracja():
         
         data = load_users_data()
         
+        #q1!11111
+        
         if data:
             max_user_id = max(int(user["user_id"]) for user in data)
             user_id = max_user_id + 1
         else:
             user_id = 1
+        user_email = next((user for user in data if user['email'] == email), None)
         
+        if user_email:
+            return jsonify({"message": "Podany email jest już zajety"}), 400
+        
+        # Sprawdzanie zgodności haseł
         if password != repeat_password:
-            return jsonify({"message": "Niezgodnosc hasel"}), 400
+            return jsonify({"message": "Niezgodność haseł"}), 400
 
-        else:
-            new_user = OrderedDict ([
-                ('user_id' , user_id),
-                ("name" , name),
-                ('lastname' , lastname),
-                ('email' , email.lower()),
-                ('password' , password),
-                ('gender' , gender)
-            ])
-    
-            data.append(new_user)
-            save_users_data(data)
+            
+        new_user = OrderedDict ([
+            ('user_id' , user_id),
+            ("name" , name),
+            ('lastname' , lastname),
+            ('email' , email.lower()),
+            ('password' , password),
+            ('gender' , gender)
+        ])
+
+        data.append(new_user)
+        save_users_data(data)
             
         return render_template("zarejestrowano.html", new_user = new_user)
     return render_template("rejestracja.html")
+
+# %% Logowanie 
+@app.route("/logowanie", methods = ["GET", "POST"])
+def logowanie():
+    
+    data = load_users_data()
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+    
+        user = next((user for user in data if user['email'] == email), None)
+    
+        if user:
+            if user['password']  == password:
+                return render_template("menu.html")
+            else:
+                return jsonify({"message": "Bledne dane logowania"})
+        else:
+            return jsonify({"message": "Brak uzytkownika o podanym emailu {email}"})
+            
+       
+    return render_template("logowanie.html")
 
 
 # %% DODAWANIE ZGLOSZENIA 
